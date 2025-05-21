a768a811ccf345972b56dbaa1e967aa9dabc0bdd
Caleb Loria Gomez <zkcaleb@Calebs-MacBook-Pro.local>
2025-05-21 03:13:02 -0600

    fix: Add get_escrow_by_contract_id function.

--- contracts/escrow/src/contract.rs
+++ contracts/escrow/src/contract.rs
@@ -104,6 +104,13 @@ impl EscrowContract {
         EscrowManager::get_escrow(e)
     }
 
+    pub fn get_escrow_by_contract_id(
+        e: Env,
+        contract_id: Address,
+    ) -> Result<Escrow, ContractError> {
+        EscrowManager::get_escrow_by_contract_id(e, &contract_id)
+    }
+
     pub fn get_multiple_escrow_balances(e: Env, addresses: Vec<Address>) -> Result<Vec<AddressBalance>, ContractError> {
         EscrowManager::get_multiple_escrow_balances(e, addresses)
     }
