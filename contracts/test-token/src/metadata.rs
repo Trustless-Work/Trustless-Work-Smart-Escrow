7d4c3aea3197735c81c8143a2ba4c4f6fce3846e
armandocodecr <totovip1464@gmail.com>
2025-06-12 23:09:11 -0600

    refactor: remove unused test-token contract and related files

--- contracts/test-token/src/metadata.rs
+++
@@ -1,7 +0,0 @@
-use soroban_sdk::Env;
-use soroban_token_sdk::{metadata::TokenMetadata, TokenUtils};
-
-pub fn write_metadata(e: &Env, metadata: TokenMetadata) {
-    let util = TokenUtils::new(e);
-    util.metadata().set_metadata(&metadata);
-}
